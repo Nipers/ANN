312e60f0f0f753263d3718e2e09c0daf48524df5
Nipers <1286043187@qq.com>
2021-10-02 21:04:12 +0800

    picture

---
+++ HW1/codes/plot.py
@@ -0,0 +1,36 @@
+import numpy as np 
+from matplotlib import pyplot as plt 
+
+acc_test = "accuracy/test/"
+acc_train = "accuracy/train/"
+loss_test = "loss/test/"
+loss_train = "loss/train/"
+
+x1 = np.arange(1,201)
+file = open(loss_train + "L_G_L_H.txt", "r")
+y1 = []
+line = file.readline()
+i = 0
+while line:
+    y1.append(float(line))
+    line = file.readline()
+# print(y1)
+file.close()
+x2 = np.arange(5,205, 5)
+file = open(loss_test + "L_G_L_H.txt", "r")
+y2 = []
+line = file.readline()
+i = 0
+while line:
+    y2.append(float(line))
+    i += 1
+    line = file.readline()
+file.close()
+plt.title('Loss') 
+plt.xlabel("epoch") 
+plt.ylabel("loss") 
+plt.plot(x1,y1, label = "train") 
+plt.plot(x2,y2, label = "test") 
+plt.legend()
+plt.savefig("T_L_G_L_H.png")
+# plt.show()
